edfd1f9eea6c1b9a01bc90b82a7bda083971b693
Quangamingvn <Quangamingvn@QUANGAMINGVN-PC>
2017-12-25 21:00:06 +0700

    Ấ

---
+++ He phuong trinh 2 an/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace He_phuong_trinh_2_an
+{
+    class HPT2an
+    {
+        double a, b, c, d, e, f, x, y;
+        public HPT2an(double aa, double bb, double cc, double dd, double ee, double ff)
+        {
+            a = aa;
+            b = bb;
+            c = cc;
+            d = dd;
+            e = ee;
+            f = ff;
+            Calcualtion();
+        }
+        private void Calcualtion()
+        {
+            y = (c * d - f * a) / (b * d - e * a);
+            x = (c - b * y) / a;
+        }
+        public double getX()
+        {
+            return x;
+        }
+        public double getY()
+        {
+            return y;
+        }
+        
+    }
+}
--- He phuong trinh 2 an/Program.cs
+++ He phuong trinh 2 an/Program.cs
@@ -6,10 +6,64 @@ using System.Threading.Tasks;
 
 namespace He_phuong_trinh_2_an
 {
+   
     class Program
     {
         static void Main(string[] args)
         {
+            double a, b, c, d, e, f;
+            #region display
+            Console.WriteLine("tinh he phuong trinh 2 an");
+            Console.WriteLine("{ a x + b y = c");
+            Console.WriteLine("{ a'x + b'y = c'");
+            #endregion
+
+            Console.Write("a=  ");
+            a = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(" {0}x + by = c", a);
+            Console.WriteLine(" a'x + b'y = c'");
+            Console.WriteLine("b= ");
+            b = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(" {0}x + {1}y = c" ,a,b);
+            Console.WriteLine(" a'x + b'y = c'");
+            Console.WriteLine("c= ");
+            c = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("{0}x + {1}y = {2}", a, b, c);
+            Console.WriteLine(" a'x + b'y = c'");
+            Console.WriteLine("d= ");
+            d = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(" {0}x + {1}y = {2}", a, b, c);
+            Console.WriteLine(" {0}x + b'y = c'",d);
+            Console.WriteLine("e= ");
+            e = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(" {0}x + {1}y = {2}", a, b, c);
+            Console.WriteLine(" {0}x + {1}y = c'",d,e);
+            Console.WriteLine("f= ");
+            f = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(" {0}x + {1}y = {2}", a, b, c);
+            Console.WriteLine(" {0}x + {1}y = {2}",d,e,f);
+            HPT2an hpt = new HPT2an(a, b, c, d, e, f);
+            if (!double.IsInfinity(hpt.getX()) && !double.IsNaN(hpt.getX()))
+            {
+                Console.WriteLine("\nResult:");
+                Console.WriteLine("X = {0}", hpt.getX());
+                Console.WriteLine("Y = {0}", hpt.getY());
+            }
+            if (double.IsInfinity(hpt.getX()))
+            {
+                Console.WriteLine("No Solution");
+            }
+            if (double.IsNaN(hpt.getX()))
+            {
+                Console.WriteLine("Infinity Solution");
+            }
+            Console.Read();
         }
     }
 }
